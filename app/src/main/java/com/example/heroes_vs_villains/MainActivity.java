2d0823ba3b67bcf2167dbae3d4e8741aeebfb23a
johny <yonatani94@gmail,com>
2020-08-20 14:57:13 +0300

    update2

--- app/src/main/java/com/example/heroes_vs_villains/MainActivity.java
+++ app/src/main/java/com/example/heroes_vs_villains/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.heroes_vs_villains;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.autofill.AutofillValue;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.SeekBar;
@@ -19,17 +21,93 @@ public class MainActivity extends AppCompatActivity {
     private ImageView main_IMG_Villains;
     private SeekBar main_SKB_Hero;
     private SeekBar main_SKB_Villains;
-
+    private int life = 300;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+            main_SKB_Hero.setMax(life);
+            main_SKB_Villains.setMax(life);
+
+        findViewById();
+
+        heroAttack();
+        disableButtonHero();
+        VillainsAttack();
+        disableButtonVillains();
+
+
+
+
+
+ }
+
+    private void disableButtonVillains() {
+        main_BTN_AttackVillains1.setEnabled(false);
+        main_BTN_AttackVillains2.setEnabled(false);
+        main_BTN_AttackVillains3.setEnabled(false);
     }
 
+    void disableButtonHero()
+    {
+        main_BTN_AttackHero1.setEnabled(false);
+        main_BTN_AttackHero2.setEnabled(false);
+        main_BTN_AttackHero3.setEnabled(false);
 
+    }
+    void changeSeekBar(SeekBar seek, int num)
+    {
+        seek.setProgress(life-num);
+    }
+    void heroAttack()
+    {
+        main_BTN_AttackHero1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeSeekBar(main_SKB_Villains,10);
+            }
+        });
+        main_BTN_AttackHero2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeSeekBar(main_SKB_Villains,30);
 
+            }
+        });
+        main_BTN_AttackHero3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeSeekBar(main_SKB_Villains,50);
 
+            }
+        });
+    }
+
+    void VillainsAttack()
+    {
+        main_BTN_AttackVillains1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeSeekBar(main_SKB_Hero,10);
+
+            }
+        });
+        main_BTN_AttackVillains2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeSeekBar(main_SKB_Hero,30);
+
+            }
+        });
+        main_BTN_AttackVillains3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeSeekBar(main_SKB_Hero,50);
+
+            }
+        });
+    }
     void findViewById()
     {
         main_BTN_AttackHero1=findViewById(R.id.main_BTN_AttackHero1);
