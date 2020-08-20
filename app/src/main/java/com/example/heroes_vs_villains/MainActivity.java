f8e42a4792b5d8553f6b15a987c450854875dccf
johny <yonatani94@gmail,com>
2020-08-20 14:29:21 +0300

    update1

--- app/src/main/java/com/example/heroes_vs_villains/MainActivity.java
+++ app/src/main/java/com/example/heroes_vs_villains/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 
 //
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +15,11 @@ public class MainActivity extends AppCompatActivity {
     private Button main_BTN_AttackVillains1;
     private Button main_BTN_AttackVillains2;
     private Button main_BTN_AttackVillains3;
+    private ImageView main_IMG_Hero;
+    private ImageView main_IMG_Villains;
+    private SeekBar main_SKB_Hero;
+    private SeekBar main_SKB_Villains;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,5 +38,9 @@ public class MainActivity extends AppCompatActivity {
         main_BTN_AttackVillains1=findViewById(R.id.main_BTN_AttackVillains1);
         main_BTN_AttackVillains2=findViewById(R.id.main_BTN_AttackVillains2);
         main_BTN_AttackVillains3=findViewById(R.id.main_BTN_AttackVillains3);
+        main_IMG_Hero=findViewById(R.id.main_IMG_Hero);
+         main_IMG_Villains=findViewById(R.id.main_IMG_Villains);
+         main_SKB_Hero=findViewById(R.id.main_SKB_Hero);
+         main_SKB_Villains= findViewById(R.id.main_SKB_Villains);
     }
 }
