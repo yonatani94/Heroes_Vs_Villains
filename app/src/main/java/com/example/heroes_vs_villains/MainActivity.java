fe545481a15651e51f104d7037ddf4db810c2575
johny <yonatani94@gmail,com>
2020-08-25 13:17:30 +0300

    update3

--- app/src/main/java/com/example/heroes_vs_villains/MainActivity.java
+++ app/src/main/java/com/example/heroes_vs_villains/MainActivity.java
@@ -1,16 +1,20 @@
 package com.example.heroes_vs_villains;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.view.autofill.AutofillValue;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.SeekBar;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
-//
+import com.bumptech.glide.Glide;
+
+
 public class MainActivity extends AppCompatActivity {
     private Button main_BTN_AttackHero1;
     private Button main_BTN_AttackHero2;
@@ -20,107 +24,294 @@ public class MainActivity extends AppCompatActivity {
     private Button main_BTN_AttackVillains3;
     private ImageView main_IMG_Hero;
     private ImageView main_IMG_Villains;
-    private SeekBar main_SKB_Hero;
-    private SeekBar main_SKB_Villains;
-    private int life = 300;
-
+    private ImageView main_IMG_background;
+    private ProgressBar main_PGB_Hero;
+    private ProgressBar main_PGB_Villains;
+    private int playerTurn=0;
+    private  boolean gameover= true;
+    private  boolean firstPlayer= true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-            main_SKB_Hero.setMax(life);
-            main_SKB_Villains.setMax(life);
+        findView();
+        addPIcWithGlide();
+        Fighter hero = new Fighter(main_PGB_Hero,"hero",true,0);
+        Fighter devil = new Fighter(main_PGB_Villains,"devil",false,0);
+        Log.d("johny", "onCreate: "+ main_PGB_Villains.getProgress());
+        Log.d("johny", "onCreate: "+ main_PGB_Hero.getProgress());
 
-        findViewById();
+            startFight(hero,devil);
 
-        heroAttack();
-        disableButtonHero();
-        VillainsAttack();
-        disableButtonVillains();
-
-
-        declareWinner();
+        Log.d("johny", "Finished!!! ");
 
 
+            Log.d("pttt", "turn is:  " + playerTurn);
 
 
  }
 
-    private void declareWinner() {
-        if(main_SKB_Villains.getProgress()==0)
+ private View.OnClickListener attackClick = new View.OnClickListener() {
+     @Override
+     public void onClick(View view) {
+
+         if(winnerIs(main_PGB_Hero,main_PGB_Villains)==1)
+         {
+             disableButton(main_PGB_Hero);
+             disableButton(main_PGB_Villains);
+         }
+
+         /*  -----------------player 1 turn ----------------------*/
+         if(playerTurn%2==0)
+         {
+             bottomClickedH(view);
+
+         }
+         /*  -----------------player 2 turn ----------------------*/
+
+         else  {
+             bottomClickedV(view);
+         }
+     }
+ };
+    private void bottomClickedH(View view) {
+
+        if(view.getTag().toString().equals("attackH1"))
         {
-            Toast.makeText(getActivity(),"Hero is winner",Toast.LENGTH_SHORT).show();
+            changeProgressBar(main_PGB_Villains,10);
+            Toast.makeText(getApplicationContext(),"Sasuke  damage 10 Hp",Toast.LENGTH_SHORT).show();
         }
-        else if(main_SKB_Hero.getProgress()==0)
-            Toast.makeText(getActivity(),"Villains is winner",Toast.LENGTH_SHORT).show();
+        else if(view.getTag().toString().equals("attackH2"))
+        {
+            changeProgressBar(main_PGB_Villains,20);
+            Toast.makeText(getApplicationContext(),"Sasuke damage 20 Hp",Toast.LENGTH_SHORT).show();
+
+        }
+        else if(view.getTag().toString().equals("attackH3"))
+        {
+            changeProgressBar(main_PGB_Villains,30);
+            Toast.makeText(getApplicationContext(),"Sasuke damage 30 Hp",Toast.LENGTH_SHORT).show();
+
+        }
+
+        Log.d("johny", "bottomClickedH: " + playerTurn);
+        playerTurn++;
+        switchButton(main_PGB_Hero);
+       // disableButton(main_PGB_Hero);
+       // enableButton(main_PGB_Villains);
+
     }
 
-    private void disableButtonVillains() {
-        main_BTN_AttackVillains1.setEnabled(false);
-        main_BTN_AttackVillains2.setEnabled(false);
-        main_BTN_AttackVillains3.setEnabled(false);
+    private void bottomClickedV(View view) {
+
+
+         if(view.getTag().toString().equals("attackV1"))
+        {
+            changeProgressBar(main_PGB_Hero,10);
+            Toast.makeText(getApplicationContext(),"Naruto damage 10 Hp",Toast.LENGTH_SHORT).show();
+
+
+        }
+        else if(view.getTag().toString().equals("attackV2"))
+        {
+            changeProgressBar(main_PGB_Hero,20);
+            Toast.makeText(getApplicationContext(),"Naruto damage 20 Hp",Toast.LENGTH_SHORT).show();
+
+        }
+        else if(view.getTag().toString().equals("attackV3"))
+        {
+            changeProgressBar(main_PGB_Hero,30);
+            Toast.makeText(getApplicationContext(),"Naruto damage 30 Hp",Toast.LENGTH_SHORT).show();
+
+        }
+        Log.d("johny", "bottomClickedV: " + playerTurn);
+        playerTurn--;
+        switchButton(main_PGB_Villains);
+        //disableButton(main_PGB_Villains);
+        //enableButton(main_PGB_Hero);
+
     }
 
-    void disableButtonHero()
+
+    @Override
+    protected void onStart() {
+        Log.d("jttt", "onStart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d("jttt", "onStop");
+        super.onStop();
+    }
+    private void switchButton(ProgressBar pgb)
     {
-        main_BTN_AttackHero1.setEnabled(false);
-        main_BTN_AttackHero2.setEnabled(false);
-        main_BTN_AttackHero3.setEnabled(false);
+        if(pgb.getTag().toString().equals("human"))
+        {
+            main_BTN_AttackHero1.setEnabled(false);
+            main_BTN_AttackHero2.setEnabled(false);
+            main_BTN_AttackHero3.setEnabled(false);
+
+            main_BTN_AttackVillains1.setEnabled(true);
+            main_BTN_AttackVillains2.setEnabled(true);
+            main_BTN_AttackVillains3.setEnabled(true);
+
+        }
+        else
+        {
+            main_BTN_AttackVillains1.setEnabled(false);
+            main_BTN_AttackVillains2.setEnabled(false);
+            main_BTN_AttackVillains3.setEnabled(false);
+
+            main_BTN_AttackHero1.setEnabled(true);
+            main_BTN_AttackHero2.setEnabled(true);
+            main_BTN_AttackHero3.setEnabled(true);
+        }
+    }
+    private void enableButton(ProgressBar pgb) {
+        if(pgb.getTag().toString().equals("human"))
+        {
+            main_BTN_AttackHero1.setEnabled(true);
+            main_BTN_AttackHero2.setEnabled(true);
+            main_BTN_AttackHero3.setEnabled(true);
+
+        }
+        else {
+            main_BTN_AttackVillains1.setEnabled(true);
+            main_BTN_AttackVillains2.setEnabled(true);
+            main_BTN_AttackVillains3.setEnabled(true);
+
+        }
+    }
+
+    private void disableButton(ProgressBar pgb) {
+        if(pgb.getTag().toString().equals("human"))
+        {
+            main_BTN_AttackHero1.setEnabled(false);
+            main_BTN_AttackHero2.setEnabled(false);
+            main_BTN_AttackHero3.setEnabled(false);
+        }
+        else {
+            main_BTN_AttackVillains1.setEnabled(false);
+            main_BTN_AttackVillains2.setEnabled(false);
+            main_BTN_AttackVillains3.setEnabled(false);
+        }
+    }
+
+    private void addPIcWithGlide() {
+        Glide
+                .with(MainActivity.this)
+                .load(R.drawable.naruto)
+                .centerCrop()
+                .into(main_IMG_Hero);
+
+        Glide
+                .with(MainActivity.this)
+                .load(R.drawable.suske)
+                .centerCrop()
+                .into(main_IMG_Villains);
+
+        Glide
+                .with(MainActivity.this)
+                .load(R.drawable.back)
+                .centerCrop()
+                .into(main_IMG_background);
 
     }
-    void changeSeekBar(SeekBar seek, int num)
+
+    private int declareWinner(Fighter hero,Fighter devil) {
+
+        if(hero.getLife().getProgress()<=0)
+        {
+            Toast.makeText(getApplicationContext(),
+                    "Hero is winner",
+                    Toast.LENGTH_LONG).show();
+            Log.d("johny","winner is hero");
+            return 1;
+        }
+        else if(devil.getLife().getProgress()<=0)
+        {
+           Toast.makeText(getApplicationContext(),"Villains is winner",Toast.LENGTH_SHORT).show();
+            Log.d("johny","winner is Villains");
+
+              return 1;
+
+        }
+        else return 0;
+
+
+    }
+
+    int winnerIs(ProgressBar pgbHero,ProgressBar pgbDevil)
     {
-        seek.setProgress(life-num);
+        Log.d("johny", "winnerIs: pgH =  " + pgbHero.getProgress());
+        Log.d("johny", "winnerIs: pgD =  " + pgbDevil.getProgress());
+        if(pgbHero.getProgress()==0)
+        {
+            Toast.makeText(getApplicationContext(),
+                    "Hero is winner!",
+                    Toast.LENGTH_SHORT).show();
+            Log.d("johny","winner is hero");
+            disableButton(pgbHero);
+            disableButton(pgbDevil);
+            return 1;
+        }
+        else if(pgbDevil.getProgress()==0)
+        {
+            Toast.makeText(getApplicationContext(),"Villains is winner!",Toast.LENGTH_SHORT).show();
+            Log.d("johny","winner is Villains");
+            disableButton(pgbHero);
+            disableButton(pgbDevil);
+            return 1;
+
+        }
+        else return 0;
     }
-    void heroAttack()
+    void changeProgressBar(ProgressBar seek, int num)
     {
-        main_BTN_AttackHero1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                changeSeekBar(main_SKB_Villains,10);
-            }
-        });
-        main_BTN_AttackHero2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                changeSeekBar(main_SKB_Villains,30);
 
+            if(seek.getProgress() <= 0.5*seek.getMax())
+            {
+                seek.getProgressDrawable().setColorFilter(ContextCompat.getColor(this,R.color.almostDying), PorterDuff.Mode.MULTIPLY);
             }
-        });
-        main_BTN_AttackHero3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                changeSeekBar(main_SKB_Villains,50);
+            else if (seek.getProgress()==0)
+            {
+                Toast.makeText(getApplicationContext(),
+                        "Game Over",
+                        Toast.LENGTH_LONG).show();
+            }
 
-            }
-        });
+            seek.setProgress(seek.getProgress()-num);
+
+        Log.d("johny", "changeProgressBar:   " + playerTurn);
+
     }
-
-    void VillainsAttack()
+    void startFight(Fighter hero,Fighter devil)
     {
-        main_BTN_AttackVillains1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                changeSeekBar(main_SKB_Hero,10);
 
-            }
-        });
-        main_BTN_AttackVillains2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                changeSeekBar(main_SKB_Hero,30);
+        if( hero.isTurn())
+        {
 
-            }
-        });
-        main_BTN_AttackVillains3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                changeSeekBar(main_SKB_Hero,50);
+            main_BTN_AttackHero1.setOnClickListener(attackClick);
+            main_BTN_AttackHero2.setOnClickListener(attackClick);
+            main_BTN_AttackHero3.setOnClickListener(attackClick);
+            devil.setTurn(true);
+            hero.setTurn(false);
 
-            }
-        });
+        }
+       if (devil.isTurn()){
+
+            main_BTN_AttackVillains1.setOnClickListener(attackClick);
+            main_BTN_AttackVillains2.setOnClickListener(attackClick);
+            main_BTN_AttackVillains3.setOnClickListener(attackClick);
+           hero.setTurn(true);
+          devil.setTurn(false);
+
+        }
     }
-    void findViewById()
+
+
+    void findView()
     {
         main_BTN_AttackHero1=findViewById(R.id.main_BTN_AttackHero1);
         main_BTN_AttackHero2=findViewById(R.id.main_BTN_AttackHero2);
@@ -129,8 +320,9 @@ public class MainActivity extends AppCompatActivity {
         main_BTN_AttackVillains2=findViewById(R.id.main_BTN_AttackVillains2);
         main_BTN_AttackVillains3=findViewById(R.id.main_BTN_AttackVillains3);
         main_IMG_Hero=findViewById(R.id.main_IMG_Hero);
-         main_IMG_Villains=findViewById(R.id.main_IMG_Villains);
-         main_SKB_Hero=findViewById(R.id.main_SKB_Hero);
-         main_SKB_Villains= findViewById(R.id.main_SKB_Villains);
+        main_IMG_Villains=findViewById(R.id.main_IMG_Villains);
+        main_PGB_Hero=findViewById(R.id.main_PGB_Hero);
+        main_PGB_Villains= findViewById(R.id.main_PGB_Villains);
+        main_IMG_background=findViewById(R.id.main_IMG_background);
     }
 }
